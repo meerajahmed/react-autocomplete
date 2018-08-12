e7bce86c26a882a99e87476a3c89c3359dd8b320
Meeraj Ahmed <meerajahmed@gmail.com>
2018-08-13 02:05:51 +0530

    searchable product data table using react

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -3,12 +3,14 @@ import React from 'react';
 const SearchBar = (props) => {
   return (
     <form>
-      <input type="text" placeholder="Search"
-             value={props.filterText}
-             onChange={(event) => props.onFilterTextChange(event.target.value)}
-      />
+      <div className="input-group input-group-lg">
+        <input type="text" placeholder="Search Category"
+               className="form-control" value={props.filterText}
+               onChange={(event) => props.onFilterTextChange(event.target.value)}/>
+      </div>
     </form>
   );
 };
 
 export default SearchBar;
+
